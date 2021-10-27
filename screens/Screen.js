9c04cc1e9dc865552771969da4b7027216154a07
amulligan <alanmugz@gmail.com>
2021-10-27 13:22:54 +0100

    F0000 - Added home, map and sos screens

--- screens/Screen.js
+++ screens/Screen.js
@@ -3,7 +3,25 @@ import { Button, ImageBackground, View, Text, StyleSheet, TouchableOpacity, Text
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+
 const API_URL = 'http://ec2-34-245-52-7.eu-west-1.compute.amazonaws.com:3000';
+
+
+function HomeScreen({ navigation, route }) {
+    return (
+        <ImageBackground source={require('../public/images/gradient-back.jpeg')} style={styles.image}>
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                <Text>Home Screen</Text>
+                <Text>{route.params.user}</Text>
+                <Button
+                    title="Sign Out"
+                    onPress={() => navigation.navigate('Login')}
+                />
+            </View>
+        </ImageBackground>
+    );
+}
+
 
 function MapScreen({ navigation }) {
     return (
@@ -11,41 +29,29 @@ function MapScreen({ navigation }) {
             <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
                 <Text>Map Screen</Text>
                 <Button
-                title="Sign Out"
-                onPress={() => navigation.navigate('Login')}
-                />
-            </View>
-        </ImageBackground>
-    );
-  }
-  
-  function SOSScreen({ navigation }) {
-    return (
-        <ImageBackground source={require('../public/images/gradient-back.jpeg')} style={styles.image}>
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <Text>SOS Screen</Text>
-                <Button
-                title="Sign Out"
-                onPress={() => navigation.navigate('Login')}
-                />
-            </View>
-    </ImageBackground>
-    );
-  }
-
-function HomeScreen({ navigation }) {
-    return (
-        <ImageBackground source={require('../public/images/gradient-back.jpeg')} style={styles.image}>
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <Text>Home Screen</Text>
-                <Button
-                title="Sign Out"
-                onPress={() => navigation.navigate('Login')}
+                    title="Sign Out"
+                    onPress={() => navigation.navigate('Login')}
                 />
             </View>
         </ImageBackground>
     );
 }
+
+
+function SOSScreen({ navigation }) {
+    return (
+        <ImageBackground source={require('../public/images/gradient-back.jpeg')} style={styles.image}>
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                <Text>SOS Screen</Text>
+                <Button
+                    title="Sign Out"
+                    onPress={() => navigation.navigate('Login')}
+                />
+            </View>
+        </ImageBackground>
+    );
+}
+
 
 function LoginScreen({ navigation }) {
 
@@ -63,20 +69,20 @@ function LoginScreen({ navigation }) {
                 'Authorization': `Bearer ${token}`,
             },
         })
-        .then(async res => { 
-            try {
-                const jsonRes = await res.json();
-                if (res.status === 200) {
-                    setMessage(jsonRes.message);
-                    navigation.navigate('Home');
-                }
-            } catch (err) {
+            .then(async res => {
+                try {
+                    const jsonRes = await res.json();
+                    if (res.status === 200) {
+                        setMessage(jsonRes.message);
+                        navigation.navigate('Root', { screen: 'Home' });
+                    }
+                } catch (err) {
+                    console.log(err);
+                };
+            })
+            .catch(err => {
                 console.log(err);
-            };
-        })
-        .catch(err => {
-            console.log(err);
-        });
+            });
     }
 
     const onSubmitHandler = () => {
@@ -91,24 +97,24 @@ function LoginScreen({ navigation }) {
             },
             body: JSON.stringify(payload),
         })
-        .then(async res => { 
-            try {
-                const jsonRes = await res.json();
-                if (res.status !== 200) {
-                    setIsError(true);
-                    setMessage(jsonRes.message + '\nPlease try again or click\nForgot Password above to reset it.');
-                } else {
-                    onLoggedIn(jsonRes.token);
-                    setIsError(false);
-                    setMessage(jsonRes.message);                 
-                }
-            } catch (err) {
+            .then(async res => {
+                try {
+                    const jsonRes = await res.json();
+                    if (res.status !== 200) {
+                        setIsError(true);
+                        setMessage(jsonRes.message + '\nPlease try again or click\nForgot Password above to reset it.');
+                    } else {
+                        onLoggedIn(jsonRes.token);
+                        setIsError(false);
+                        setMessage(jsonRes.message);
+                    }
+                } catch (err) {
+                    console.log(err);
+                };
+            })
+            .catch(err => {
                 console.log(err);
-            };
-        })
-        .catch(err => {
-            console.log(err);
-        });
+            });
     };
 
     const getMessage = () => {
@@ -124,22 +130,23 @@ function LoginScreen({ navigation }) {
                     <View style={styles.inputs}>
                         <TextInput style={styles.input} placeholder="Email" autoCapitalize="none" onChangeText={setEmail}></TextInput>
                         <TextInput secureTextEntry={true} style={styles.input} placeholder="Password" onChangeText={setPassword}></TextInput>
-                        <Text style={[styles.message, {color: isError ? 'red' : 'green'}]}>{message ? getMessage() : null}</Text>
+                        <Text style={[styles.message, { color: isError ? 'red' : 'green' }]}>{message ? getMessage() : null}</Text>
                         <TouchableOpacity style={styles.button} onPress={onSubmitHandler}>
                             <Text style={styles.buttonText}>Login</Text>
                         </TouchableOpacity>
                         <TouchableOpacity style={styles.buttonAlt} onPress={() => navigation.navigate('SignUp')}>
                             <Text style={styles.buttonAltText}>{'Dont have an account? Sign Up'}</Text>
                         </TouchableOpacity>
-                        <TouchableOpacity style={styles.buttonAlt}>
+                        <TouchableOpacity style={styles.buttonAlt} onPress={() => navigation.navigate('Root', { screen: 'Home', params: { user: "Jane" }})}>
                             <Text style={styles.buttonAltText}>{'Forgot Password'}</Text>
                         </TouchableOpacity>
-                    </View>    
+                    </View>
                 </View>
             </View>
         </ImageBackground>
     );
 };
+
 
 function SignUpScreen({ navigation }) {
 
@@ -163,23 +170,23 @@ function SignUpScreen({ navigation }) {
             },
             body: JSON.stringify(payload),
         })
-        .then(async res => { 
-            try {
-                const jsonRes = await res.json();
-                if (res.status !== 200) {
-                    setIsError(true);
-                    setMessage(jsonRes.message);
-                } else {
-                    setIsError(false);
-                    setMessage(jsonRes.message + '\nPlease return to login screen');
-                }
-            } catch (err) {
+            .then(async res => {
+                try {
+                    const jsonRes = await res.json();
+                    if (res.status !== 200) {
+                        setIsError(true);
+                        setMessage(jsonRes.message);
+                    } else {
+                        setIsError(false);
+                        setMessage(jsonRes.message + '\nPlease return to login screen');
+                    }
+                } catch (err) {
+                    console.log(err);
+                };
+            })
+            .catch(err => {
                 console.log(err);
-            };
-        })
-        .catch(err => {
-            console.log(err);
-        });
+            });
     };
 
     const getMessage = () => {
@@ -196,14 +203,14 @@ function SignUpScreen({ navigation }) {
                         <TextInput style={styles.input} placeholder="Email" autoCapitalize="none" onChangeText={setEmail}></TextInput>
                         <TextInput style={styles.input} placeholder="Name" onChangeText={setName}></TextInput>
                         <TextInput secureTextEntry={true} style={styles.input} placeholder="Password" onChangeText={setPassword}></TextInput>
-                        <Text style={[styles.message, {color: isError ? 'red' : 'green'}]}>{message ? getMessage() : null}</Text>
+                        <Text style={[styles.message, { color: isError ? 'red' : 'green' }]}>{message ? getMessage() : null}</Text>
                         <TouchableOpacity style={styles.button} onPress={onSubmitHandler}>
                             <Text style={styles.buttonText}>Sign Up</Text>
                         </TouchableOpacity>
                         <TouchableOpacity style={styles.buttonAlt} onPress={() => navigation.navigate('Login')}>
                             <Text style={styles.buttonAltText}>{'Return to Login'}</Text>
                         </TouchableOpacity>
-                    </View>    
+                    </View>
                 </View>
             </View>
         </ImageBackground>
@@ -216,7 +223,7 @@ const styles = StyleSheet.create({
         flex: 1,
         width: '100%',
         alignItems: 'center',
-    },  
+    },
     card: {
         flex: 1,
         backgroundColor: 'rgba(255, 255, 255, 0.4)',
@@ -245,13 +252,13 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         paddingTop: '10%',
-    },  
+    },
     input: {
         width: '80%',
         borderBottomWidth: 1,
         borderBottomColor: 'black',
         paddingTop: 10,
-        fontSize: 16, 
+        fontSize: 16,
         minHeight: 40,
     },
     button: {
@@ -289,9 +296,10 @@ const styles = StyleSheet.create({
     },
 });
 
+
 const Tab = createBottomTabNavigator();
 
-function HomeTabs() {
+function Root() {
     return (
         <Tab.Navigator>
             <Tab.Screen name="Home" component={HomeScreen} />
@@ -301,16 +309,22 @@ function HomeTabs() {
     );
 }
 
+
 const Stack = createNativeStackNavigator();
 
 function Screen() {
     return (
         <Stack.Navigator initialRouteName="Login">
-          <Stack.Screen name="Login" component={LoginScreen} options={{title: "Login"}} />
-          <Stack.Screen name="SignUp" component={SignUpScreen} options={{title: "SignUp"}} />
-          <Stack.Screen name="HomeTabs" component={HomeTabs} />
+            <Stack.Screen
+                name="Root"
+                component={Root}
+                options={{ headerShown: false }}
+            />
+            <Stack.Screen name="Login" component={LoginScreen} options={{ title: "Login" }} />
+            <Stack.Screen name="SignUp" component={SignUpScreen} options={{ title: "SignUp" }} />
         </Stack.Navigator>
     );
 }
+
 
 export default Screen;
